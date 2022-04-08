00495214a955c46a45dbf0a290326d44ac7c654b
yunjung <yunjj21@hs.ac.kr>
2022-04-09 01:50:33 +0900

    백준17626_Four Squares 풀이

---
+++ 백준17626_Four Squares.cpp
@@ -0,0 +1,21 @@
+// น้มุ 17626 : Four Squares (DP)
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int dp[50001] = { 0 };
+	dp[1] = 1;
+	for (int i = 1; i <= n; i++) {
+		dp[i] = dp[1] + dp[i - 1];
+		for (int j = 2; j * j <= i; j++) {
+			dp[i] = min(dp[i], 1 + dp[i - j * j]);
+		}
+	}
+	cout << dp[n] << endl;
+}
